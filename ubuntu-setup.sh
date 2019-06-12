85a94173270ed3a34126d13152477e45f5ed91d6
matias garafoni <mgarafoni@clearrtb.com>
2019-06-12 15:26:46 -0300

    remove frontend to build docker stufff

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
 apt update && apt install build-essential -y && apt install apache2 -y && apt install virtualenv -y && apt install libapache2-mod-wsgi python-dev -y && apt -y install nano && apt -y install git && apt install -y wget && apt install zlib1g-dev -y
 apt install libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev libtk8.5 libgdm-dev libdb4o-cil-dev libpcap-dev  -y # need this for pip (ssl download)
